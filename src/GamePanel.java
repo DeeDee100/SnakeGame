8b55fdfeccb25ee57f02df2712c974df1a99f117
DeeDee100 <amanda_tinoco00@hotmail.com>
2023-07-11 22:40:20 -0300

    add screen for game

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,76 @@
 import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
     
+    static final int WIDTH = 600;
+    static final int HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (WIDTH * HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 70;
+    
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+
+    int bodyParts = 5;
+    int foodEaten;
+    int foodX;
+    int foodY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+    GamePanel(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.darkGray);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+    }
+
+    public void startGame() {
+        newFood();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        
+    }
+
+    public void move() {
+
+    }
+
+    public void newFood(){}
+
+    public void checkFood(){}
+
+    public void checkCollisions(){}
+    
+    public void gameOver(Graphics g){}
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){}
+    }
 }
