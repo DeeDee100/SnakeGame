9ce25ff29abdadfe92001a3ee5505eacbdc659fe
DeeDee100 <amanda_tinoco00@hotmail.com>
2023-07-12 14:36:12 -0300

    add movimento da cobrinha | add comida

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -35,6 +35,7 @@ public class GamePanel extends JPanel implements ActionListener {
         this.setBackground(Color.darkGray);
         this.setFocusable(true);
         this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
 
     public void startGame() {
@@ -48,25 +49,78 @@ public class GamePanel extends JPanel implements ActionListener {
         super.paintComponent(g);
         draw(g);
     }
-
+    
     public void draw(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillOval(foodX, foodY, UNIT_SIZE, UNIT_SIZE);
+
+        for(int i=0; i<bodyParts; i++){
+            if(i == 0){
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else{
+                g.setColor(Color.orange);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
         
     }
 
     public void move() {
+        for(int i=bodyParts;i>0;i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+
+        switch (direction) {
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+    
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+    
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
 
     }
 
-    public void newFood(){}
+    public void newFood(){
+        foodX = random.nextInt((int)(WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        foodY = random.nextInt((int)(HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+    }
 
     public void checkFood(){}
 
-    public void checkCollisions(){}
+    public void checkCollisions(){
+        for(int i=bodyParts; i>0; i--){
+            if((x[0] == x[i]) && (y[0] == y[i])){
+                running = false;
+            }
+        }
+
+        
+    }
     
     public void gameOver(Graphics g){}
     
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(running){
+            move();
+            checkFood();
+            checkCollisions();
+        }
+
+        repaint();
     }
 
     public class MyKeyAdapter extends KeyAdapter{
